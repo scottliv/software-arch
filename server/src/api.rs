def8782a0d2109a766bd68ee3f20e96cb363ca17
scottliv <scottdlivingstone@gmail.com>
2024-03-16 17:01:01 -0700

    refactor template to return both images

--- server/src/api.rs
+++ server/src/api.rs
@@ -1,10 +1,13 @@
 use actix_web::{error::InternalError, get, web};
-use database::entity::generated_image::Entity as GeneratedImage;
-use sea_orm::{DatabaseConnection, DbBackend, EntityTrait, Statement};
+use database::entity::generated_image::{Entity as GeneratedImage, Model as GeneratedImageModel};
+use database::entity::inspiration_image::{
+    Entity as InspirationImage, Model as InspirationImageModel,
+};
+use sea_orm::{DatabaseConnection, DbBackend, DbErr, EntityTrait, Statement};
 
 use crate::template::GeneratedImageTemplate;
 
-#[get("/generated_images/{id}")]
+#[get("/images/{id}")]
 pub async fn get_image_by_id(
     db: web::Data<DatabaseConnection>,
     id: web::Path<String>,
@@ -21,25 +24,16 @@ pub async fn get_image_by_id(
         .one(db.as_ref())
         .await;
 
-    if image.is_err() {
-        return Err(InternalError::new(
-            "Error reading image from db".to_string(),
-            actix_web::http::StatusCode::from_u16(500).unwrap(),
-        ));
-    }
+    let (generated_image, inspiration_image) =
+        get_generated_and_inspiration_image(image, db.as_ref()).await?;
 
-    match image.unwrap() {
-        Some(image) => Ok(GeneratedImageTemplate {
-            generated_image: image,
-        }),
-        None => Err(InternalError::new(
-            "Image not found".to_string(),
-            actix_web::http::StatusCode::from_u16(404).unwrap(),
-        )),
-    }
+    Ok(GeneratedImageTemplate {
+        generated_image,
+        inspiration_image,
+    })
 }
 
-#[get("/generated_images/{id}/next")]
+#[get("/images/{id}/next")]
 pub async fn get_next_image(
     db: web::Data<DatabaseConnection>,
     id: web::Path<String>,
@@ -60,42 +54,69 @@ pub async fn get_next_image(
         ))
         .one(db.as_ref())
         .await;
+    let (generated_image, inspiration_image) =
+        get_generated_and_inspiration_image(image, db.as_ref()).await?;
 
-    if image.is_err() {
-        return Err(InternalError::new(
-            "Error reading image from db".to_string(),
-            actix_web::http::StatusCode::from_u16(500).unwrap(),
-        ));
-    }
-
-    match image.unwrap() {
-        Some(image) => Ok(GeneratedImageTemplate {
-            generated_image: image,
-        }),
-        None => Err(InternalError::new(
-            "Image not found".to_string(),
-            actix_web::http::StatusCode::from_u16(404).unwrap(),
-        )),
-    }
+    Ok(GeneratedImageTemplate {
+        generated_image,
+        inspiration_image,
+    })
 }
 
-#[get("/generated_images/first")]
+#[get("/images/first")]
 pub async fn get_first_image(
     db: web::Data<DatabaseConnection>,
 ) -> askama::Result<GeneratedImageTemplate, InternalError<String>> {
     let image = GeneratedImage::find().one(db.as_ref()).await;
+    let (generated_image, inspiration_image) =
+        get_generated_and_inspiration_image(image, db.as_ref()).await?;
 
-    if image.is_err() {
-        return Err(InternalError::new(
-            "Error reading image from db".to_string(),
-            actix_web::http::StatusCode::from_u16(500).unwrap(),
-        ));
-    }
+    Ok(GeneratedImageTemplate {
+        generated_image,
+        inspiration_image,
+    })
+}
 
-    match image.unwrap() {
-        Some(image) => Ok(GeneratedImageTemplate {
-            generated_image: image,
-        }),
+async fn get_generated_and_inspiration_image(
+    image: Result<Option<GeneratedImageModel>, DbErr>,
+    db: &DatabaseConnection,
+) -> Result<(GeneratedImageModel, InspirationImageModel), InternalError<String>> {
+    let image = match image {
+        Ok(image) => image,
+        Err(_) => {
+            return Err(InternalError::new(
+                "Error reading image from db".to_string(),
+                actix_web::http::StatusCode::from_u16(500).unwrap(),
+            ));
+        }
+    };
+
+    let image = match image {
+        Some(image) => image,
+        None => {
+            return Err(InternalError::new(
+                "Image not found".to_string(),
+                actix_web::http::StatusCode::from_u16(404).unwrap(),
+            ));
+        }
+    };
+
+    let inspiration_image = InspirationImage::find_by_id(image.inspiration_image_id)
+        .one(db)
+        .await;
+
+    let inspiration_image = match inspiration_image {
+        Ok(inspiration_image) => inspiration_image,
+        Err(_) => {
+            return Err(InternalError::new(
+                "Error reading image from db".to_string(),
+                actix_web::http::StatusCode::from_u16(500).unwrap(),
+            ));
+        }
+    };
+
+    match inspiration_image {
+        Some(inspo_image) => Ok((image, inspo_image)),
         None => Err(InternalError::new(
             "Image not found".to_string(),
             actix_web::http::StatusCode::from_u16(404).unwrap(),
--- server/src/template.rs
+++ server/src/template.rs
@@ -1,12 +1,14 @@
 use askama_actix::Template;
 use database::entity::generated_image::Model as GeneratedImageModel;
+use database::entity::inspiration_image::Model as InspirationImageModel;
 
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate;
 
 #[derive(Template)]
-#[template(path = "generated-image.html")]
+#[template(path = "images.html")]
 pub struct GeneratedImageTemplate {
     pub generated_image: GeneratedImageModel,
+    pub inspiration_image: InspirationImageModel,
 }
